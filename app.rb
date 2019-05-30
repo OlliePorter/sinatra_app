3450ec88f0b93a344ba982f0e0ce6012b5a3c899
Ollie Porter <ol.porter@me.com>
2019-05-30 12:57:48 +0100

    Randomising names in the contoller and presenting in named-cat view

--- app.rb
+++ app.rb
@@ -4,7 +4,17 @@ get '/' do
   "Hello World"
 end
 
-get '/cat' do
+get '/random-cat' do
   @random_name = ['Amigo','Oscar','Viking'].sample
   erb(:index)
+end
+
+post '/named-cat' do
+  p params
+  @random_name = params[:name]
+  erb(:index)
+end
+
+get '/cat_form' do
+  erb(:cat_form)
 end
